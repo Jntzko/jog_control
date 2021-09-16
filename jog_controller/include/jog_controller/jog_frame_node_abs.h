62b2f50e4e566eed6ec46c27c3ae802ea8086ffd
Yannick Jonetzko <jonetzko@informatik.uni-hamburg.de>
2021-09-16 14:10:33 +0200

    clang format

--- jog_controller/include/jog_controller/jog_frame_node_abs.h
+++ jog_controller/include/jog_controller/jog_frame_node_abs.h
@@ -1,61 +1,60 @@
 #ifndef JOG_FRAME_ABS_H
 #define JOG_FRAME_ABS_H
 
-#include <string>
-#include <ros/ros.h>
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
-#include <mutex>
+#include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <interactive_markers/interactive_marker_server.h>
 #include <jog_msgs/JogFrameAbs.h>
-#include <sensor_msgs/JointState.h>
-#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_state/robot_state.h>
 #include <moveit_msgs/GetPositionFK.h>
 #include <moveit_msgs/GetPositionIK.h>
-#include <control_msgs/FollowJointTrajectoryAction.h>
-#include <actionlib/client/simple_action_client.h>
+#include <mutex>
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include <string>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
-#include <interactive_markers/interactive_marker_server.h>
 
-typedef actionlib::SimpleActionClient < control_msgs::FollowJointTrajectoryAction > TrajClient;
+typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>
+    TrajClient;
 
-namespace jog_frame
-{
+namespace jog_frame {
 
-typedef struct
-{
+typedef struct {
 public:
   std::string action_ns;
   std::string type;
   std::vector<std::string> joints;
-  
+
 } Controller;
 
 /**
  * Class JogFrameNodeAbs - Provides the jog_frame for absolute poses
  */
-class JogFrameNodeAbs
-{
+class JogFrameNodeAbs {
 public:
   /**
    * @breif: Default constructor for JogFrameNodeAbs Class.
    */
-  JogFrameNodeAbs ();
-  void jog_frame_cb (jog_msgs::JogFrameAbsConstPtr msg);
-  void joint_state_cb (sensor_msgs::JointStateConstPtr msg);
+  JogFrameNodeAbs();
+  void jog_frame_cb(jog_msgs::JogFrameAbsConstPtr msg);
+  void joint_state_cb(sensor_msgs::JointStateConstPtr msg);
   int get_controller_list();
   void update();
   void getFkPose();
   void jogStep();
   void publishPose(sensor_msgs::JointState state);
+
 protected:
   ros::Subscriber joint_state_sub_, jog_frame_sub_;
   ros::ServiceClient fk_client_, ik_client_;
 
   std::map<std::string, Controller> cinfo_map_;
-  std::map<std::string, TrajClient*> traj_clients_;
-  std::map<std::string,ros::Publisher> traj_pubs_;
+  std::map<std::string, TrajClient *> traj_clients_;
+  std::map<std::string, ros::Publisher> traj_pubs_;
 
   std::map<std::string, double> joint_map_;
   geometry_msgs::PoseStamped pose_stamped_;
@@ -65,6 +64,10 @@ protected:
   bool use_action_;
   bool intermittent_;
   bool publish_tf_;
+
+  robot_model_loader::RobotModelLoader *robot_model_loader_;
+  moveit::core::RobotModelPtr robot_model_;
+  moveit::core::JointModelGroup *joint_model_group_;
 
   jog_msgs::JogFrameAbsConstPtr ref_msg_;
   std::mutex ref_msg_mutex_;
